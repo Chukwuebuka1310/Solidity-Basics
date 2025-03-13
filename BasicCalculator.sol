9b2bb088fe468f939b52ea124f5d7e004f7ecf16
Chukwuebuka Godsfavour <agugodsfavour@gmail.com>
2025-03-13 12:31:53 +0100

    Initial commit

---
+++ BasicCalculator.sol
@@ -0,0 +1,37 @@
+// SPDX-License-Identifier: MIT 
+
+pragma solidity ^0.8.7;
+
+contract BasicCalculator{
+
+    //int allows for negative  integers
+    int256 result;
+
+    function add() public {
+        //Adds 20 to the result
+        result += 20;
+    }
+
+    function subtraction() public {
+        //Code
+        //Subtracts 10 from the result
+        result -= 10;
+    }
+
+    function multiply() public {
+        //Code
+        //Multiplies the result by 2
+        result *= 2;
+    }
+
+    function division() public {
+        //Code
+        //Divides the result by 5
+        result /= 5;
+    }
+
+    function get() public view returns(int256) {
+        //Returns the value of calculation done
+        return result;
+    }
+}
