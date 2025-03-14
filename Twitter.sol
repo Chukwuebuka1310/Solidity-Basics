18e5ff845825343ca84652d78404c8e89687a9ea
Chukwuebuka Godsfavour <agugodsfavour@gmail.com>
2025-03-14 19:07:45 +0100

    Added little ideas of array to the mapping and also a function that get all tweets

--- Twitter.sol
+++ Twitter.sol
@@ -3,23 +3,35 @@ pragma solidity ^0.8.7;
 
 contract Twitter{
 
-    // Declaration of variables
-    address public authorAddress;
-    string public tweet;
+   // Declaration of variables
+   address public authorAddress;
+   string public tweet;
 
-    // Assigns a tweet to the user of the account
-    mapping(address => string) public authorToTweet;
+   // Declaration of mapping(Unlike the prevous declaration, this mapping has an array)
+   // string[] rep an array, to get a response when the mapping is called, you need to give it an address of the sender ansd also an index number(0,1,2,3) for a particular element
+   mapping(address => string[]) public authorToTweet;
 
-    //Function that requires a parameter and create a tweet with that parameter
+
+   //Function that requires a parameter and create a tweet with that parameter
    function createTweet(string memory _tweet) public {
 
       tweet = _tweet;
-      
       //msg.sender gives us an access to the address of the sender.
       authorAddress = msg.sender;
 
       //Mapping that assigns the sender address to their tweets from declaration of line 11
-      authorToTweet[msg.sender] = _tweet;
+      //1. Without string[](array of string)
+      // authorToTweet[msg.sender] = _tweet;
+
+      //2. with string[]
+      authorToTweet[msg.sender].push(_tweet);
+   }
+
+   //Function that get all tweets of a particular user
+   function getTweetsByUserAddress(address _user) public view returns(string[] memory ){
+      
+      //goes to the mapping and get all the tweet by a particular user
+      return authorToTweet[_user];
    }
 
 }
